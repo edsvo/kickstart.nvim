3fa31d1075964bc67b2910adfb302a4c146b7778
Ed Svoboda <49281259+edsvo@users.noreply.github.com>
2025-05-31 17:09:38 -0700

    Change colorscheme to Tokyo Night Storm

--- init.lua
+++ init.lua
@@ -886,6 +886,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        style = 'storm',
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -894,7 +895,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
